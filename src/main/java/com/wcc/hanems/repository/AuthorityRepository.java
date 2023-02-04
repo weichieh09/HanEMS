ab44079a22c9c67efa55e41943e5391c760a80ea
weichieh09 <weichieh09@gmail.com>
2023-02-04 22:08:43 +0800

    Initial version of HanEMS generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/wcc/hanems/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.wcc.hanems.repository;
+
+import com.wcc.hanems.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/wcc/hanems/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.wcc.hanems;
+
+import com.wcc.hanems.HanEmsApp;
+import com.wcc.hanems.config.AsyncSyncConfiguration;
+import com.wcc.hanems.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { HanEmsApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
