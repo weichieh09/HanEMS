3474ecd36f874cee3acdff7969371846d270df04
weichieh09 <weichieh09@gmail.com>
2023-02-05 10:38:15 +0800

    [add] Wcc104

--- src/main/webapp/app/router/customized.ts
+++ src/main/webapp/app/router/customized.ts
@@ -2,6 +2,7 @@ import { Authority } from '@/shared/security/authority';
 
 const Wcc102Component = () => import('@/customized/wcc102/wcc102.vue');
 const Wcc103Component = () => import('@/customized/wcc103/wcc103.vue');
+const Wcc104Component = () => import('@/customized/wcc104/wcc104.vue');
 
 export default [
   {
@@ -13,5 +14,10 @@ export default [
     path: '/wcc103',
     name: 'Wcc103',
     component: Wcc103Component,
+  },
+  {
+    path: '/wcc104',
+    name: 'Wcc104',
+    component: Wcc104Component,
   },
 ];
